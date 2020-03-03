90d37188c40141b9a81a9b96efd7f59056dd6217
yndmn <dem139@mail.ru>
2020-03-03 11:54:40 +0300

    * Added hotkeys

--- text_editor/text_editor.py
+++ text_editor/text_editor.py
@@ -33,25 +33,41 @@ def save_as():
     except Exception:
         showerror(title = 'Ошибка', message = 'Невозможно сохранить файл')
 
+def selectAll(event):
+    window.after(10, select_all, event.widget)
+
+def select_all(widget):
+    widget.selection_range(0, END)
+    widget.icursor(END)
+
 window = tkinter.Tk()
 window.title("Simple text editor")
-window.minsize(width = 20, height = 20)
-window.maxsize(width = 1920, height = 1080)
-text = tkinter.Text(window, width = 800, height = 600, bg = 'black', fg = 'white')
-text.pack()
+window.minsize(width = 400, height = 400)
+window.maxsize(width = 800, height = 800)
+text = tkinter.Text(window, width = 100, height = 100, bg = 'black', fg = 'white', font = ('Trebuchet MS', 12))
+text.pack(side = tkinter.LEFT)
+
+# scroll = tkinter.Scrollbar(window, command = text.yview, orient = tkinter.VERTICAL)
+# scroll.pack(side = tkinter.RIGHT, fill = tkinter.Y)
+
 menuBar = tkinter.Menu(window)
 fileMenu = tkinter.Menu(menuBar)
 fileMenu.add_command(label = 'Создать файл', command = create_new_file)
 fileMenu.add_command(label = 'Открыть файл', command = open_file)
 fileMenu.add_command(label = 'Сохранить', command = save_file)
 fileMenu.add_command(label = 'Сохранить как', command = save_as)
-fileMenu.add_separator()
 fileMenu.add_command(label = 'Выход', command = window.quit)
 menuBar.add_cascade(label = 'Файл', menu = fileMenu)
+
 window.config(menu = menuBar)
+# text.config(yscrollcommand = scroll.set)
+
+select_all_hotkey = tkinter.Entry(width = 40)
+select_all_hotkey.focus_set()
+select_all_hotkey.pack()
+select_all_hotkey.bind('<Control-a>', selectAll)
+
+window.event_add('<<Copy>>', '<Control-ntilde>')
+window.event_add('<<Paste>>', '<Control-igrave>')
 
 window.mainloop()
-
-## TODO: Добавить горячие клавиши;
-##       добавить скроллбар;
-##       глянуть как работают теги
